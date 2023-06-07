d010c8f7e7956526ef73e6e681caec2a346fb8f5
Trần Chánh Trực <36126386+tctruc79@users.noreply.github.com>
2023-06-08 00:40:07 +0700

    Add files via upload

---
+++ R/run_scDHA.R
@@ -0,0 +1,278 @@
+# Load libraies ----
+
+# scDHA library
+library(scDHA)
+
+library(tidyverse)
+library(tidyquant)
+library(tools)
+library(readr)
+
+# Functions to perform scDHA demo ----
+
+# Load data from file
+load_data <- function(dsname = NULL, file = NULL) {
+
+    # Load data from file or package
+    if (!is.null(dsname)) {
+        
+        # load from built-in dataset
+        
+        data_from_source <- get(dsname)
+        
+        # Get data matrix and label
+        
+        if (!is.null(data_from_source)) {
+            data <- t(data_from_source[["data"]]) 
+            label <- as.character(data_from_source[["label"]])
+            cell.stages <- unique(label)
+            cell.ids <- colnames(data_from_source[["data"]])
+        }
+        
+    } else if ((!is.null(file)) && (file.exists(file))) {
+    
+       
+        if (file_ext(file) == "rds") {
+            
+            # load .rds file
+            
+            data_from_source <- readr::read_rds(file)    
+        
+            # Get data matrix and label
+        
+            if (!is.null(data_from_source)) {
+                data <- t(data_from_source@assays[["data"]]@listData[[1]]) 
+                label <- as.character(data_from_source$cell_type1)
+                cell.stages <- unique(label)
+                cell.ids <- colnames(data_from_source@assays[["data"]]@listData[[1]])
+            }
+            
+        } else if (file_ext(file) == "rda") {
+            
+            # load .rda file
+            data_from_source <- get(load(file))
+            
+            # Get data matrix and label, same to built-in dataset
+            
+            if (!is.null(data_from_source)) {
+                data <- t(data_from_source[["data"]]) 
+                label <- as.character(data_from_source[["label"]])
+                cell.stages <- unique(label)
+                cell.ids <- colnames(data_from_source[["data"]])
+            }
+            
+        } else if (file_ext(file) == "csv"){
+            
+            # load .csv file
+            # file = "data/csv/camp-brain-fix_imputed.csv"
+            data_from_source <- readr::read_csv(file = file, num_threads = 16)
+        
+            # Get data matrix 
+            
+            if (!is.null(data_from_source)) {
+                data <- t(data_from_source)
+                label <- NULL
+                cell.stages <- NULL
+                
+                col_names <- c(sprintf("Cell_%d", seq(1, ncol(data))))
+                colnames(data) <- col_names
+                cell.ids <- col_names
+                
+                data <- t(data)
+            }
+            
+        } else if (file_ext(file) == "tsv"){        
+            
+            # load .tsv file
+            
+            data_from_source <- readr::read_tsv(file = file, num_threads = 16)
+            
+            # Get data matrix 
+            
+            if (!is.null(data_from_source)) {
+                data <- t(data_from_source)
+                label <- NULL
+                cell.stages <- NULL
+                
+                col_names <- c(sprintf("Cell_%d", seq(1, ncol(data))))
+                colnames(data) <- col_names
+                cell.ids <- col_names
+                
+                data <- t(data)
+            }
+        }        
+    }
+    
+       
+    # Transform the data
+    data <- log2(data + 1)
+    
+    sc <- list("data" = data, 
+               "label" = label,
+               "cell.stages" = cell.stages,
+               "cell.ids" = cell.ids)
+    
+    return(sc)
+}
+
+# sc <- load_data(dsname = "Goolam")
+
+
+# SC data after go through scDHA pipeline
+scDHA_pipeline <- function(sc) {
+ 
+    # SC data after go through scDHA pipeline
+    result <- scDHA(sc$data, ncores = 16, seed = 1)
+    
+    return(result)
+}
+
+# scDHA_result <- scDHA_pipeline(sc)
+
+
+# scDHA function generates clustering result, the input matrix has rows as samples and columns as genes
+
+# cell_segregation <- function(sc, scDHA_result) {
+#     
+#     # SC data after go through scDHA pipeline
+#     result <- sc %>% scDHA_pipeline()     
+#     
+#     # cell segregation and bind with cell ID    
+#     cluster <- bind_cols(sc$cell.ids, scDHA_result$cluster) %>% set_names(c("Cell ID", "Cluster ID"))
+#     
+#     return(cluster)
+# }
+
+
+cell_segregation <- function(sc, scDHA_result) {
+
+    # cell segregation and bind with cell ID    
+    cluster <- bind_cols(sc$cell.ids, scDHA_result$cluster) %>% set_names(c("Cell ID", "Cluster ID"))
+    
+    return(cluster)
+}
+
+# cluster <- cell_segregation(sc, scDHA_result)
+
+# Generate 2D representation, the input is the output from scDHA function
+gen_2d_rep <- function(scDHA_result) {
+ 
+     result <- scDHA.vis(scDHA_result, ncores = 16, seed = 1)
+     
+     return(result)
+}
+
+# rep2d <- gen_2d_rep(scDHA_result)
+
+
+# Plot the representation of the dataset, different colors represent different cell types
+
+visual_2d <- function(result) {
+    
+    plot(result$pred, col=factor(sc$label), xlab = "scDHA1", ylab = "scDHA2")
+}
+
+# visual_2d(rep2d)
+
+# Generate pseudo-time for each cell, the input is the output from scDHA function
+
+pseudo_time <- function(scDHA_result) {
+    result <- scDHA.pt(scDHA_result, start.point = 1, seed = 1)
+    
+    return(result)
+}
+
+
+# Calculate R-squared value representing correlation between inferred pseudo-time and cell stage order
+cal_r2 <- function (sc, result) {
+    
+    r2 <- round(cor(result$pt, as.numeric(factor(sc$label, levels = sc$cell.stages)))^2, digits = 2)
+    
+    return(r2)
+}
+
+
+# Plot pseudo-temporal ordering of cells in dataset
+
+pseudo_plot <- function(sc, result) {
+ 
+    plot(result$pt, factor(sc$label, levels = sc$cell.stages), xlab= "Pseudo Time", ylab = "Cell Stages", xaxt="n", yaxt="n")
+    axis(2, at=1:5,labels=sc$cell.stages, las=2)
+    text(x = 1, y = 4.5, labels = paste0("R2 = ", r2))
+}
+
+
+# Cell Classification
+
+# Predict the labels of cells in testing set, the input matrices have rows as samples and columns as genes
+
+cell_classification <- function(train.x, train.y, test.x) {
+    
+    prediction <- scDHA.class(train = train.x, train.label = train.y, test = test.x, seed = 1)
+
+    return(prediction)
+}
+
+# Calculate accuracy of the predictions
+
+cal_accuracy <- function(test.y, prediction) {
+    
+    accuracy <- round(sum(test.y == prediction)/length(test.y), 2)
+    
+    return(accuracy)
+}
+
+# test the code
+
+# set.seed(1)
+# 
+# idx <- sample.int(nrow(sc$data), size = round(nrow(sc$data)*0.75))
+# 
+# train.x <- sc$data[idx, ]
+# train.y <- sc$label[idx]
+# test.x  <- sc$data[-idx, ]
+# test.y  <- sc$label[-idx]
+# test.cell.ids <- sc$cell.ids[-idx] 
+# 
+# # Predict the labels of cells in testing set, the input matrices have rows as samples and columns as genes
+# 
+# prediction <- cell_classification (train.x, train.y, test.x) 
+# 
+# # Calculate accuracy
+# accuracy <<- cal_accuracy (test.y, prediction)
+# 
+# pre_result <- bind_cols(test.cell.ids, prediction, test.y) %>% set_names(c("Cell ID", "Predicted Label", "True Label")) %>%
+#     mutate(`Accurate` = case_when(
+#     `Predicted Label` == `True Label` ~ 'TRUE',
+#     TRUE ~ 'FALSE'
+#     ))
+
+
+
+# Convert .csv to .tsv
+
+csv_to_tsv <-  function(file1, file2) {
+
+    data <- readr::read_csv(file1)
+
+    readr::write_tsv(data, file2)
+}
+
+
+
+
+# test convert
+
+#csv_to_tsv("data/csv/goolam_imputed.csv", "data/tsv/google_imputed.tsv")
+
+# data1 <- readr::read_tsv("data/tsv/google_imputed.tsv")
+
+
+
+# Load rda file
+# file <- "data/rda/Goolam.rda"
+
+# file.exists("canteen_clean.rda")
+# filename <- file.choose("Goolam")
+# Goolam <- readRDS(filename)
+
--- README.md
+++ README.md
@@ -1,1 +1,103 @@
-# scDHA_Shiny
+# Overview
+
+Based on the proposed scDHA framework, this is a simple Shiny application that will give an illustration about how the scDHA framework works.
+
+![scDHA Shiny](scDHA_Shiny.png)
+
+<br>
+<br>
+
+# GitHub
+
+The full source code is located in GitHub at this URL:
+https://github.com/tctruc79/scDHA_Shiny.git
+          
+<br>
+<br>
+
+# How it works
+
+## 1. Select Datasource
+
+You are able to select the datasources from built-in datasets in the scDHA packages, or you can choose to upload your dataset files. 
+
+The extentions supported are: .rds, .rda, .csv, .tsv.
+
+For example:
+
+.rds: Goolam.rds, files in download data.
+.rda: Goolam.rda, files in download data.
+.csv: goolam_imputed.csv, files in imputed folder
+.tsv: goolam_imputed.csv, converted from imputed.csv files, using csv_to_tsv function in *run_scDHA.R* file in this project.
+
+<br>
+
+## 2. Select Dataset
+
+### 2.1. Built-in datasets:
+
+You will want to select a dataset from application's listed down all scDHA's built-in datasets, i.e. "Goolam".
+
+<br>
+
+### 2.2. Upload files:
+
+You will also be able to select "Upload Dataset Files" and browse to the file located in your local machine.
+
+<br>
+
+## 3. Load Data
+
+Next, press **"Load Data"** button to load data from above selected datasource. The data will be loaded to the table on the top area of the screen, "Single-Cell Data".
+
+<br>
+
+## 4. scDHA Autoencode
+
+The data before doing analysis, it should be normalized, dimension reduced, denoised by pre-process through a scHDA pipeline. Press button **"Autoencode"** to do this.
+
+<br>
+
+## 5. Segregation
+
+After normalized the data, we do cell segregation, press button **"1. Segregation"** to perform this.
+
+The result will be shown in the first portion on the bottom area "1. Cell Segregation", this will list cells by ID and the Cluster ID that the cell is belong to.
+
+<br>
+
+## 6. Visualization
+
+To visualize the segregation result, press button **"2. Visualization"**, a second portion on the bottom will show the plot that illustrates how the groups of cell types are well separated to each other.
+
+<br>
+
+## 7. Classification, 
+
+Next application of scDHA framework is the capability to classify the cells with high accuracy and low variance. Press button **"3. Classification"** to see the result shown in the third portion of the bottom area.
+
+This table will list cells with their IDs, the predicted label, compared to its true label and also the accuracy ratio.
+
+
+<br>
+
+## 8. Time Trajectory
+
+The last application of scDHA shown in this Shiny app is the pseudo-time trajectory of the cell developmental stages. This can be obtained by pressing on button **"4. Time Trajectory"**.
+
+
+<br>
+
+
+## 9. Save results
+
+After running the analysis, you are able to save the result, press **"Save Result"** button to get this.
+
+
+<br>
+
+## 10. Reset
+
+To do analysis with new data, you should reset all the variables, configuration to the default values. **"Reset"** button will have you to do that.
+
+
